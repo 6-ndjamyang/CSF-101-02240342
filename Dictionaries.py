1d43e67e0759fc9762988ffbc90c643cc00729fe
RameshThapa56 <150133744+RameshThapa56@users.noreply.github.com>
2024-09-02 09:17:56 +0600

    First push

---
+++ Dictionaries.py
@@ -0,0 +1,7 @@
+person_info = {
+  "name": "jamyang ngawang dorji",
+  "age": "22",
+  "height": "1.00",
+  "is_student" : "True",
+}
+print(person_info)
---
+++ python.py
@@ -0,0 +1,24 @@
+x = 5
+x = 5
+print(x)
+
+
+
+
+
+mathmarks = 2
+englishmarks = 3.5
+name = 'Dorji'
+PI = 3.14159
+
+doublemarks = mathmarks * 2
+total = mathmarks + englishmarks
+print(total)
+
+
+money = 100
+
+if money <= 300:
+        print('i am rich')
+else:
+        print('i am poor')
